9d6cd999588303847f973d3a779f9a44c6b9afd4
Jeremy Rader <jscottr8r@gmail.com>
2019-02-08 21:16:10 -0700

    paginate calls to swapi

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 const express = require('express')
+const request = require('request-promise')
+
 const app = express()
 const port = 3000
 
-app.get('/people', (req, res) => {
-    res.send('got the peoples')
+app.get('/people', async (req, res) => {
+    let people = await fetchAllFromSwapi('people')
+    res.send(people)
 })
 
-app.get('/planets', (req, res) => {
-    res.send('Gustav Holst - Jupiter')
+app.get('/planets', async (req, res) => {
+    let planets = await request.get('https://swapi.co/api/planets')
+    res.send(planets)
 })
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
+
+const fetchAllFromSwapi = async (resource) => {
+
+    let results = []
+
+    let url = `https://swapi.co/api/${resource}`
+    
+    do {
+        var resourcePage = await request.get(url, { json: true })
+        
+        results = results.concat(resourcePage.results)
+        url = resourcePage.next
+    }
+    while(resourcePage.next)
+
+    return results
+
+}
