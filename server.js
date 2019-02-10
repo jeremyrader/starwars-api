3543c9c5f8df25455687a11f086026b392cee1ea
Jeremy Rader <jscottr8r@gmail.com>
2019-02-10 13:30:01 -0700

    sort people on name, height, mass

--- server.js
+++ server.js
@@ -6,6 +6,24 @@ const port = 3000
 
 app.get('/people', async (req, res) => {
     let people = await fetchAllFromSwapi('people')
+
+    if (req.query.sortBy && ['name', 'height', 'mass'].includes(req.query.sortBy)) {
+        people.sort((a,b) => {
+
+            aNum = parseFloat(a[req.query.sortBy].replace(/,/g, ''))
+            bNum = parseFloat(b[req.query.sortBy].replace(/,/g, ''))
+
+            if (isNaN(aNum)) {
+                return 1
+            }
+            else if (isNaN(bNum)) {
+                return -1
+            }
+
+            return aNum - bNum
+        })
+    }
+
     res.send(people)
 })
 
